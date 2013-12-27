04171dca6ef2b11186db9c47a965c78c7333c111
tkrugg <tkrugg@live.fr>
2013-12-27 13:55:11 +0100

    Added linkedin crawler spec

---
+++ crawlers/Linkedin.py
@@ -0,0 +1,16 @@
+
+
+def getLocation(user):
+  # do something with the API
+
+  location["country"] = ""
+  location["city"] =""
+  location["zipcode"] = ""
+  return location
+
+
+def getEducation(user):
+  # do something with API
+  education = ""
+  return education
+

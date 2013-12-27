862c907f097395e0a0ca512de1198ac13cdaf324
tkrugg <tkrugg@live.fr>
2013-12-27 13:59:20 +0100

    minor change in crawler for linkedin

--- crawlers/Linkedin.py
+++ crawlers/Linkedin.py
@@ -3,14 +3,16 @@
 def getLocation(user):
   # do something with the API
 
-  location["country"] = ""
-  location["city"] =""
-  location["zipcode"] = ""
+  location = {}
+
+  location["country"] = "" # change
+  location["city"] = ""     # change
+  location["zipcode"] = "" #change
   return location
 
 
 def getEducation(user):
   # do something with API
-  education = ""
+  education = ""         # change
   return education
 

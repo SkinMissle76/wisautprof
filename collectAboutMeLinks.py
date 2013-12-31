79c017498affb53e9f8834d33bb3db859b56a494
tkrugg <tkrugg@live.fr>
2013-12-31 10:24:26 +0100

    fetched more aboutme profiles

--- collectAboutMeLinks.py
+++ collectAboutMeLinks.py
@@ -14,28 +14,25 @@ apiKeys = [
 bc = Bing(apiKeys)
 
 def getLocationsToSearch():
+  #counties = getAllCountiesToSearch()
+  #return counties
+  return getOtherLocationKeywordsToSearch()
+
+def getAllCitiesToSearch():
   c_db = CountiesDB()
   cities = c_db.getAllCities()
 
-  #sortedCities = sorted(cities, key = lambda c : c["city"])
-  #return sortedCities
-  counties = map(lambda c : c["county"], c_db.getAllCounties())
-  print counties
-
-def getAllCitiesToSearch()
-  c_db = CountiesDB()
-  cities = c_db.getAllCities()
-  
   sortedCities = sorted(cities, key = lambda c : c["city"])
   return sortedCities
 
 def getAllCountiesToSearch():
   c_db = CountiesDB()
   counties = map(lambda c : c["county"], c_db.getAllCounties())
-  print counties
+  return counties
 
 def getOtherLocationKeywordsToSearch():
-  return ["United Kingdom", "Great Britain", "UK", "GB"]
+  #return ["United Kingdom", "Great Britain", "UK", "GB", "England", "Wales", "Scotland"]
+  return ["United Kingdom","England", "Wales", "Scotland"]
 
 
 
@@ -63,7 +60,8 @@ def getLink(linkObject, location):
 
 
 def searchLocation(location, startPage, numberOfPages):
-  query = location + " linkedin twitter site:http://about.me"
+  query = '"' + location + '"' + ' site:http://about.me'
+  print query
   results = bc.search(query, startPage, numberOfPages)
   return results
 
@@ -82,13 +80,14 @@ def searchAllLocation():
   #    db.add(str(username), r)
   #return results
 
-  start = 502
+  start = 0
   locations = getLocationsToSearch()
   nbOfLocations = len(locations)
+  "About to search for", nbOfLocations, "locations"
   for i, c in enumerate(locations):
-    if i > start:
-      print "now searching for profiles from", c["city"], str(i) + "/" + str(nbOfLocations)
-      results = searchLocation(c["city"], 0, 10)
+    if i >= start:
+      print "now searching for profiles from", c, str(i) + "/" + str(nbOfLocations)
+      results = searchLocation(c, 0, 100)
       formattedResults = map(lambda r : getLink(r, c), results)
       for r in formattedResults:
         username = r["username"]
@@ -99,8 +98,7 @@ def searchAllLocation():
 
 
 
-#results = searchAllLocation()
-getLocationsToSearch()
+results = searchAllLocation()
 
 
 

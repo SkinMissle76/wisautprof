eac8988f04a18f69d5251871b5488744ca7974f5
tkrugg <tkrugg@live.fr>
2014-01-05 17:07:16 +0100

    small refactoring in sentiment classifier

--- classifiers/SentimentPolarityEmotion.py
+++ classifiers/SentimentPolarityEmotion.py
@@ -1,31 +1,11 @@
-
-
-import requests, json
-import cStringIO
-
-#url =
-#-H "AUTHENTICATION:8a77503e17148fb5f3d5fd7d1e80ce86c23b6133" \
-#-H "ACCEPT:application/json" \
-#-H "VERSION:2.2" \
-#http://api.lymbix.com/tonalize \
-#-d "article=He was happy and surprised instead of being an angry guy. \
-#            Although he wasn't too happy about it he said yes anyways! \
-#            What do you think of this decision? \
-#    &return_fields=[] \
-#    &reference_id=1243122"
-
-
-import pycurl
-from pprint import pprint
-
-
+import cStringIO, pycurl, json
 
 class SentimentPolarityEmotionClassifier:
   _curl = None
   def __init__(self, apikey):
     self._curl = pycurl.Curl()
     self._curl.setopt(self._curl.URL, 'http://api.lymbix.com/tonalize')
-    self._curl.setopt(self._curl.HTTPHEADER, ['Accept:application/json', "AUTHENTICATION:8a77503e17148fb5f3d5fd7d1e80ce86c23b6133","VERSION:2.2"])
+    self._curl.setopt(self._curl.HTTPHEADER, ['Accept:application/json', "AUTHENTICATION:" + apikey,"VERSION:2.2"])
 
   def analyse(self, text):
     response = cStringIO.StringIO()
@@ -35,7 +15,4 @@ class SentimentPolarityEmotionClassifier:
     output = response.getvalue()
     asdictOutput = json.loads(output)
     return asdictOutput
-
-
-
 

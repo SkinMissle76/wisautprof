7ec303f93f67e38e281270caa42284c56ab3f820
tkrugg <tkrugg@live.fr>
2014-01-08 22:35:50 +0100

    Added final database for users

---
+++ models/FinalDBUsers.py
@@ -0,0 +1,22 @@
+
+import shelve
+
+
+DATA_DIR = "./data/"
+FILE_NAME = "final_db_users_aboutme.shelve"
+
+
+class FinalDBUsers:
+  _db = None
+
+  def __init__(self, filename = FILE_NAME):
+    self._db = shelve.open(DATA_DIR + filename)
+
+  def add(self, twitterId, age, gender, education, location, tweets = []):
+    self._db[twitterId] = {
+      "age" : age,
+      "gender" : gender,
+      "education" : education,
+      "location" : location,
+      "tweets" : tweets
+    }
